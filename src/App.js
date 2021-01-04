afc958ab636a57ccf9be32581bb8571d8368d022
jokale <kalemajoanna@gmail.com>
2021-01-04 13:58:17 +0000

    fix component karma and team builder

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends React.Component {
       <h3 id="skinny">Reliable, effecient delivery</h3>
       <h3 id="bold">Powered by Technology</h3>
       <h6 id="skinny"> Our Artificial intelligence powered by tools use millions of porject data <br></br>points to ensure that your project is successful</h6>
-      {/* <Karma/>
-      <Calculator/>
-      <Supervisor/>
-      <Teambuilder/> */}
+      <Karma/>
+      {/* <Calculator/>
+      <Supervisor/> */}
+      <Teambuilder/>
     </div>
   );
 }
